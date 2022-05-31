131f10c652dfdc8754c95f1a29ba91301d00bf3e
matovu-farid <matovu90@gmail.com>
2022-05-31 14:17:39 +0300

    Create Greeting component and redux store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-
+import messageReducer from './message'
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    message: messageReducer,
   },
 });
